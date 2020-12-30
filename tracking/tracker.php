1d32d515e8682fe73b5c5a63ff427e59c13192f0
Aymane Hrouch <jo.aymane@gmail.com>
2020-12-30 14:57:36 +0100

    add folder variable

--- tracking/tracker.php
+++ tracking/tracker.php
@@ -2,32 +2,20 @@
 
 date_default_timezone_set("Africa/Casablanca");
 
-$handle = fopen("./tracking/counter.txt", "r");
+$handle = fopen("./".$folder."/counter.txt", "r");
 $counter = (int ) fread($handle,20);
 fclose ($handle);
 
 if(!isset($_GET['counter'])) {
     $counter++;
-    $handle = fopen("./tracking/counter.txt", "w" );
+    $handle = fopen("./".$folder."/counter.txt", "w" );
     fwrite($handle,$counter) ;
     fclose ($handle);
     
-    $visits = fopen("./tracking/visits.txt", "a");
+    $visits = fopen("./".$folder."/visits.txt", "a");
     $data = $counter."- date: ".date("Y/m/d, H:i")." | ip: ".$_SERVER['REMOTE_ADDR']."\n";
     fwrite($visits,$data) ;
     fclose ($visits) ;
-}
-else if($_GET['counter'] == 'reset') {
-    $counter = 0;
-    $handle = fopen("./tracking/counter.txt", "w" );
-    fwrite($handle,$counter) ;
-    fclose ($handle) ;
-}
-else if($_GET['counter'] == -1) {
-	$counter--;
-    $handle = fopen("./tracking/counter.txt", "w" );
-    fwrite($handle,$counter) ;
-    fclose ($handle) ;
 }
 
 ?>
